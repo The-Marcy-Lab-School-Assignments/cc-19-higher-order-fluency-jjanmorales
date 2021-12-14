0175e94368aaea6e1301d12b83b652ea40356637
Jan Morales <moralesjan40@gmail.com>
2021-12-14 15:44:00 +0000

    Completed all

--- index.js
+++ index.js
@@ -35,5 +35,21 @@ const addSchool = arr => arr.map(person => {
 
 const allUseJavaScript = arr => arr.every(person => person.language === 'JavaScript');
 
-console.log(allUseJavaScript(alumni));
+// console.log(allUseJavaScript(alumni));
 
+const animals =['cheetah','dog', 'cat', 'dodobird', 'bear', 'dolphin']
+
+const noVowel = arr => arr.map(animal => animal.replace(/[aeiou]/gi, 'x'));
+console.log(noVowel(animals));
+
+const onlyVowelA = arr => arr.filter(animal => animal.includes('a'));
+console.log(onlyVowelA(animals));
+
+const longerThanSeven = arr => arr.some(animal => animal.length > 7);
+console.log(longerThanSeven(animals));
+
+const allFour = arr => arr.every(animal => animal.length === 4);
+console.log(allFour(animals));
+
+const concatStrings = arr => arr.join(" ");
+console.log(concatStrings(animals));
